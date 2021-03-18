83464edc7a3b2e9fe12736e4d842acfeb8c2d7fe
Stenver Suurkütt <stenver.ss@gmail.com>
2021-03-18 15:13:47 +0200

    added pop-form and functions to add new books to library

--- script.js
+++ script.js
@@ -9,37 +9,42 @@ Book.prototype.info = function() {
     return this.title + " by " + this.author + ", " + this.numberOfPages + " pages, " + this.readOrNot
 }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book)
-}
-
-let myLibrary = []
 
 const book1 = new Book("The Martian", "Andy Weir", 369, "not read yet")
 const book2 = new Book("12 rules for life", "Jordan Peterson", 448, "read")
 
-addBookToLibrary(book1)
-addBookToLibrary(book2)
+let myLibrary = [book1, book2]
 
 let content = ""
 
 const main = document.getElementById('container')
 const newBookBtn = document.getElementById('newBook')
 
-for (let i = 0; i<myLibrary.length; i++) {
-    content = ""
-    /*for (let prop in myLibrary[i]) {
-        if (myLibrary[i].hasOwnProperty(prop)) {
-            content += myLibrary[i][prop] + " "   
-        }
-    }*/
-    content = myLibrary[i].info()
+function addBookToLibrary(book) {
+    myLibrary.push(book)
+    content = myLibrary[myLibrary.length-1].info()
     let newDiv = document.createElement('div')
     newDiv.className = "card"
     newDiv.textContent = content
     main.appendChild(newDiv)
-     
 }
+
+function createLibrary() {
+    for (let i = 0; i<myLibrary.length; i++) {
+        content = ""
+        /*for (let prop in myLibrary[i]) {
+            if (myLibrary[i].hasOwnProperty(prop)) {
+                content += myLibrary[i][prop] + " "   
+            }
+        }*/
+        content = myLibrary[i].info()
+        let newDiv = document.createElement('div')
+        newDiv.className = "card"
+        newDiv.textContent = content
+        main.appendChild(newDiv)
+    }
+}
+createLibrary()    
 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
@@ -47,4 +52,29 @@ function openForm() {
   
 function closeForm() {
     document.getElementById("myForm").style.display = "none";
+}
+
+function newBook() {
+    let newTitle = document.getElementById('title').value
+    let newAuthor = document.getElementById('author').value
+    let newPages = document.getElementById('pages').value
+    let newSwitch = document.getElementById('switch').value
+    if (newSwitch == "off") {
+        newSwitch = "not read yet"
+    } else {
+        newSwitch = "read"
+    }
+    let newBook = new Book(newTitle, newAuthor, newPages, newSwitch)
+    addBookToLibrary(newBook)
+    closeForm()
+}
+
+
+function toggle() {
+    let onOff = document.getElementById('switch').value
+    if (onOff == "off") {
+        document.getElementById('switch').value = "on"
+    } else {
+        document.getElementById('switch').value = "off"
+    }
 }
